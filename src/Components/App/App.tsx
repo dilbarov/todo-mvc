2d881a63bb4a18c81fec32443e185ec5e8c8eac1
Jack <jackdilbarov@gmail.com>
2019-11-18 19:11:58 +0500

    webpack + react + typescript

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+import * as styles from './App.less';
+
+class App extends React.Component {
+
+    handler = () => {
+        return "Hello";
+    };
+
+    render() {
+        return (
+            <div>
+                <div className={styles.label}>My React App! {`${this.handler()?.length}`}</div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        app: ['./src/index.tsx']
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    mode: "none",
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'less-loader',
+                    'typed-css-modules-loader'
+                ]
+            },
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+    devtool: "source-map",
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        })
+    ]
+};
