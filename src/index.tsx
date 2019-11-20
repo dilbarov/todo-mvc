8d41ca9be38233a5e1f1dddf91fead7db5624786
Jack <jackdilbarov@gmail.com>
2019-11-20 19:37:53 +0500

    index.ts in App component was added

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from "./Components/App/App";
+import App from "./Components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
