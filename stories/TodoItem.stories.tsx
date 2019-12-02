08218eb2482a225a4a0646db7181dcdd46b4e423
jack <dilbarov@skbkontur.ru>
2019-12-02 17:12:33 +0500

    refactor storybook

--- stories/TodoItem.stories.tsx
+++ stories/TodoItem.stories.tsx
@@ -1,16 +1,39 @@
 import * as React from "react";
-
+import { action } from "@storybook/addon-actions";
 import {TodoItem} from "../src/Components/TodoItem/TodoItem";
 
-export default {title: "Button"};
+export default {title: "TodoItem"};
 
-export const simpleCase = () => {
+export const simpleItem = () => {
     return (
-        <TodoItem value={ {id: '123', description: "sadfsdf", completed: false } }
+        <TodoItem value={{id: '123', description: "sadfsdf", completed: false}}
                   onChange={() => {
+                      action("Change")
                   }}
                   onDelete={() => {
+                      action("Delete")
                   }}/>
 
+    )
+};
+
+export const twoItem = () => {
+    return (
+        <>
+            <TodoItem value={{id: '123', description: "sadfsdf", completed: false}}
+                      onChange={() => {
+                          action("Change")
+                      }}
+                      onDelete={() => {
+                          action("Delete")
+                      }}/>
+            <TodoItem value={{id: '456', description: "sadasdfsdf", completed: false}}
+                      onChange={() => {
+                          action("Change")
+                      }}
+                      onDelete={() => {
+                          action("Delete")
+                      }}/>
+        </>
     )
 };
